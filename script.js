20378d4da9717e2aad5358b3015b1a611a7a4d30
oybek003 <ojbekmuslimov1@gmail.com>
2025-01-11 15:40:16 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const newDiv = document.createElement('h1');
+newDiv.textContent = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique magni beatae harum sequi natus ipsam soluta repudiandae molestiae nobis, totam, qui quibusdam suscipit unde atque vel excepturi tempora at, sed debitis! Totam rem distinctio vel officiis optio soluta reprehenderit! Perferendis non adipisci quia? Quod doloribus mollitia quasi qui nobis architecto animi fuga quidem possimus atque repellendus, temporibus modi iure optio saepe, sed ex quae, repudiandae eum? Soluta impedit velit consequuntur eius esse nemo accusamus autem maxime fuga quae qui incidunt dolore iure rerum voluptates provident, quod corporis, at nihil saepe, nobis cum omnis. Eos magnam atque temporibus aspernatur recusandae, modi quasi explicabo vitae laudantium obcaecati repellendus voluptas dicta ipsam ducimus necessitatibus soluta facere quam! Tempore, laudantium odit ducimus sed fuga pariatur reiciendis voluptatum molestias dolore nostrum modi beatae quas exercitationem. Facilis praesentium vero voluptate beatae similique fugiat harum facere alias eos voluptatibus consectetur labore earum ipsam nesciunt, assumenda nostrum error omnis, hic recusandae quos eius officiis quaerat! Temporibus ducimus distinctio voluptatibus quibusdam, reprehenderit nisi saepe similique aliquid, officiis odit vero facere consectetur. Sint quo ratione sed maiores labore est repudiandae ipsa praesentium a asperiores amet iste dolores earum hic nobis, deleniti consequuntur rerum impedit nostrum quae esse natus. Ullam tempora consectetur illo quos enim mollitia nihil laborum eum molestiae officia alias reprehenderit accusantium provident, veniam commodi eveniet unde amet, consequuntur quae pariatur laudantium ratione corrupti. Quam, facere suscipit tempora modi ipsum commodi recusandae natus nostrum unde veritatis molestiae ex animi reprehenderit iure autem nisi. Optio vel nisi, voluptatum magni sint possimus. Et tempore laborum temporibus rem ut beatae, error accusamus qui similique est! Ipsa illum quod omnis, molestias ducimus tempora illo quis natus iure. Sit similique fugiat molestiae dignissimos reiciendis ea ratione corrupti minima deleniti excepturi provident sapiente, nostrum, ipsa, quo aperiam itaque iste dolore quae rerum autem eaque neque! Maxime earum, incidunt eius unde hic cupiditate doloremque nisi? Enim aliquid vero doloribus quis ullam voluptate id ut dolore quos consequatur perspiciatis culpa sunt, sed amet quasi quaerat accusamus modi debitis! Facere voluptas maiores reiciendis, iste aperiam, accusamus reprehenderit fugiat dolorem consequatur maxime expedita nisi laboriosam quas molestiae est? Est possimus praesentium cumque minima deleniti autem ut magni? Eveniet corrupti odit voluptatibus, officia labore quae esse. Blanditiis ab non sequi ";
+
+document.body.appendChild(newDiv);
+
+const backgroundColor = prompt("yozing bg");
+        if (backgroundColor) {
+            document.body.style.backgroundColor = backgroundColor;
+        }
+
+
+const textColor = prompt("yozing");
+
+if(textColor){
+    document.body.style.color = textColor;
+}
+
+const Size = prompt("size kiriting")
+
+if(Size){
+    document.body.style.fontSize = Size
+}
